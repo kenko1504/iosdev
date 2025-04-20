79f265e95c98df836a4919656446272ea9bea651
kenko1504 <kenkowatanabe1504@gmail.com>
2025-04-20 17:25:41 +1000

    updated ui

---
+++ iosDev/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  iosDev
+//
+//  Created by Kenji Watanabe on 20/4/2025.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
