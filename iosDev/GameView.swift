7ac3fc60bee025deb3a8d7826728656f2862c937
kenko1504 <kenkowatanabe1504@gmail.com>
2025-04-20 20:18:06 +1000

    timer is working properly now

--- iosDev/GameView.swift
+++ iosDev/GameView.swift
@@ -6,31 +6,46 @@
 //
 
 import SwiftUI
-
 struct GameView: View {
-    //seems like @State must be called first before @Binding to avoid unintended behaviour
     @State private var score: Int = 0
-    //receives the binded data "timeLimit" from content view
+    @State private var localTimeLimit: Double = 0
+    @State private var timerRunning: Bool = true
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    // This will only be used to get the initial value
     @Binding var timeLimit: Double
 
     var body: some View {
-        VStack{
-            HStack{
-                Text("Time: \(Int(timeLimit))")
+        VStack {
+            HStack {
+                Text("Time: \(Int(localTimeLimit))")
+                    .onReceive(timer) { _ in
+                        if localTimeLimit > 0 && timerRunning {
+                            localTimeLimit -= 1
+                        } else {
+                            timerRunning = false
+                        }
+                    }
                 Spacer()
-                Text("Score: \(Int(score))")
+                Text("Score: \(score)")
             }
             .padding(.horizontal, 20.0)
+
             Spacer()
-            Button("test"){
+
+            Button("Test") {
+                // Test logic
             }
         }
-
+        .onAppear {
+            localTimeLimit = timeLimit // Copy once on appear
+        }
     }
 }
 
 struct GameView_Previews: PreviewProvider {
-    static var previews: some View{
+    static var previews: some View {
         GameView(timeLimit: .constant(10))
     }
 }
